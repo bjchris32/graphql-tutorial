3def0247548c2dbacb5b89fa8a1bd3480eb44361
bjchris32 <bjchris32@gmail.com>
2024-05-29 15:43:25 -0500

    create link model and its graphql resolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,5 +9,14 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+    field :all_links, [LinkType], null: false
+
+    # this method is invoked, when `all_link` fields is being resolved
+    def all_links
+      Link.all
+    end
   end
 end
