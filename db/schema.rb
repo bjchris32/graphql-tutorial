f48f6ff256284228ce564d07bd3114ec31ddac34
bjchris32 <bjchris32@gmail.com>
2024-06-03 11:43:00 -0500

    feat: belongs to relationship between user and links

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_30_205541) do
+ActiveRecord::Schema.define(version: 2024_06_03_160323) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
