158ff8f1913cef72c102ebe083a3176ccb76397a
bjchris32 <bjchris32@gmail.com>
2024-06-03 11:02:29 -0500

    test: add the session key

--- test/graphql/mutations/sign_in_user_test.rb
+++ test/graphql/mutations/sign_in_user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Mutations::SignInUserTest < ActiveSupport::TestCase
   def perform(args = {})
-    Mutations::SignInUser.new(object: nil, field: nil, context: {}).resolve(args)
+    Mutations::SignInUser.new(object: nil, field: nil, context: { session: {} }).resolve(args)
   end
 
   def create_user
