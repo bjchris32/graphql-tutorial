0562fb8f8b4006779c960e91df0fbc634b15545f
bjchris32 <bjchris32@gmail.com>
2024-06-06 12:28:01 -0500

    feat: add search feature

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,11 +5,6 @@ module Types
 
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, [LinkType], null: false
-
-    # this method is invoked, when `all_link` fields is being resolved
-    def all_links
-      Link.all
-    end
+    field :all_links, resolver: Resolvers::LinksSearch
   end
 end
